f0e51de3eeb846ff9e506f9ee06e94641c65ee80
CAMPO RANGEL Karol Josef <karol-josef.campo@soprasteria.com>
2023-05-15 10:14:55 +0200

    Added sum and substract endpoint into Controller

--- src/main/java/com/java/calculator/controller/CalculatorController.java
+++ src/main/java/com/java/calculator/controller/CalculatorController.java
@@ -1,10 +1,33 @@
 package com.java.calculator.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.java.calculator.service.OperateService;
 
 @RequestMapping("calculator")
 @RestController
 public class CalculatorController {
 
+	@Autowired
+	private OperateService operateService;
+	
+	@GetMapping(path = "sum/{number1}/{number2}", produces = "application/json")
+	public ResponseEntity<Double> getSum(@PathVariable Double number1, @PathVariable Double number2) {
+		Double result = operateService.getSum(number1, number2);
+		return new ResponseEntity<>(result, HttpStatus.OK);
+		
+	}
+	
+	@GetMapping(path = "subtract/{number1}/{number2}", produces = "application/json")
+	public ResponseEntity<Double> getSubtract(@PathVariable Double number1, @PathVariable Double number2) {
+		Double result = operateService.getSubtract(number1, number2);
+		return new ResponseEntity<>(result, HttpStatus.OK);
+		
+	}
 }
