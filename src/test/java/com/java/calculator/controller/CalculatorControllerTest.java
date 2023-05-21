8085c3233547df51006a154b146efe0b3a5cd6a0
CAMPO RANGEL Karol Josef <karol-josef.campo@soprasteria.com>
2023-05-21 15:55:47 +0200

    Increased test coverage as well as corrected the implementation added in the controller to check for null values.

--- src/test/java/com/java/calculator/controller/CalculatorControllerTest.java
+++ src/test/java/com/java/calculator/controller/CalculatorControllerTest.java
@@ -1,13 +1,18 @@
 package com.java.calculator.controller;
 
 import static org.assertj.core.api.Assertions.assertThatNullPointerException;
+import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -17,20 +22,22 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import com.java.calculator.service.OperateService;
+import com.java.calculator.service.OperateServiceImpl;
 
 @WebMvcTest
 @AutoConfigureMockMvc
+@RunWith(MockitoJUnitRunner.class)
 public class CalculatorControllerTest {
 	
 	@Autowired
 	private MockMvc mockMvc;
-
-	@InjectMocks
-	private CalculatorController calculatorController;
 	
 	@MockBean
-	private OperateService operateService;
+	private OperateServiceImpl operateService;
+	
+	@Autowired
+	@InjectMocks
+	private CalculatorController calculatorController;
 	
 	@BeforeEach
 	public void setUp() throws Exception{
@@ -42,7 +49,7 @@ public class CalculatorControllerTest {
 	public void testCalculatorSumOk() throws Exception{
 		String URL = "/sum/{number1}/{number2}";
 		
-		Mockito.when(operateService.getSum(Double.valueOf(2), Double.valueOf(2))).thenReturn(Double.valueOf(4));
+		when(operateService.getSum(Double.valueOf(2), Double.valueOf(2))).thenReturn(Double.valueOf(4));
 		
 		ResponseEntity<Double> sumResult = calculatorController.getSum(Double.valueOf(2), Double.valueOf(2));
 		
@@ -56,19 +63,18 @@ public class CalculatorControllerTest {
 	public void testCalculatorSubstractOk() throws Exception{
 		String URL = "/subtract/{number1}/{number2}";
 		
-		Mockito.when(operateService.getSubtract(Double.valueOf(4), Double.valueOf(2))).thenReturn(Double.valueOf(2));
+		when(operateService.getSubtract(Double.valueOf(4), Double.valueOf(2))).thenReturn(Double.valueOf(2));
 		
-		ResponseEntity<Double> sumResult = calculatorController.getSubtract(Double.valueOf(4), Double.valueOf(2));
+		ResponseEntity<Double> subtractResult = calculatorController.getSubtract(Double.valueOf(4), Double.valueOf(2));
 		
 		this.mockMvc
 			.perform(MockMvcRequestBuilders.get(URL, Double.valueOf(4), Double.valueOf(2)))
-			.andExpect(result -> assertEquals(Double.valueOf(2), sumResult.getBody()));
-		
+			.andExpect(result -> assertEquals(Double.valueOf(2), subtractResult.getBody()));
 	}
 	
 	
 	@Test
-	public void testCalculatorSumKO() throws Exception{
+	public void testCalculatorSumKO_1() throws Exception{
 		Mockito.when(operateService.getSum(Double.valueOf(2), Double.valueOf(2))).thenReturn(null);
 		
 		assertThatNullPointerException();
@@ -76,9 +82,54 @@ public class CalculatorControllerTest {
 	}
 	
 	@Test
-	public void testCalculatorSubstractKO() throws Exception{
+	public void testCalculatorSubstractKO_1() throws Exception{
 		Mockito.when(operateService.getSubtract(Double.valueOf(4), Double.valueOf(2))).thenReturn(null);
 		
 		assertThatNullPointerException();
 	}
+	
+	@Test
+	public void testCalculatorSumKO_2() throws Exception{
+		Mockito.when(operateService.getSum(Double.valueOf(2), null)).thenThrow(NullPointerException.class);
+		
+		Assertions.assertThrows(NullPointerException.class ,
+				() -> {
+					calculatorController.getSum(Double.valueOf(4), null);
+				});
+		
+	}
+	
+	@Test
+	public void testCalculatorSubstractKO_2() throws NullPointerException, Exception{
+		
+		Mockito.when(operateService.getSubtract(Double.valueOf(2), null)).thenThrow(new NullPointerException());
+		
+		Assertions.assertThrows(NullPointerException.class ,
+				() -> {
+					calculatorController.getSubtract(Double.valueOf(2), null);
+				});
+	}
+	
+	
+	@Test
+	public void testCalculatorSumKO_3() throws Exception{
+		Mockito.when(operateService.getSum(null, Double.valueOf(2))).thenThrow(NullPointerException.class);
+		
+		Assertions.assertThrows(NullPointerException.class ,
+				() -> {
+					calculatorController.getSum(null, Double.valueOf(4));
+				});
+	}
+	
+	@Test
+	public void testCalculatorSubstractKO_3() throws Exception{
+		Mockito.when(operateService.getSubtract(null, Double.valueOf(2))).thenThrow(NullPointerException.class);
+		
+		Assertions.assertThrows(NullPointerException.class ,
+				() -> {
+					calculatorController.getSubtract(null, Double.valueOf(4));
+				});
+		
+	}
+	
 }
